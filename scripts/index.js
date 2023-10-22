902245da58d7dfe352ad49527e547415d2fe64ac
Marina Student <mahbianchi8@gmail.com>
2023-10-21 22:06:55 -0300

    added fonts

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,28 @@
+let popup = document.querySelector(".popup");
 let editPopupButton = document.querySelector(".profile__edit-button");
 let closePopupButton = document.querySelector(".popup__close-icon");
-let popup = document.querySelector(".popup");
-let saveButton = document.querySelector(".popup__button-submit");
 
 editPopupButton.addEventListener("click", function () {
-    popup.classList.add("active")
+    popup.classList.add("popup_opened");
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
 });
 
 closePopupButton.addEventListener("click", function() {
-    popup.classList.remove("active")
+    popup.classList.remove("popup_opened")
 });
 
-let form = document.querySelector("popup__form-input",
+let form = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__form-input_name"); 
+let aboutInput = document.querySelector(".popup__form-input_about");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__job");
+
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector(".popup__form-input_name");
-    let jobInput = document.querySelector(".popup__form-input_about");
-    popup.style.display = "none";
-});
-
-form.addEventListener('submit', handleProfileFormSubmit); 
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
+    popup.classList.remove("popup_opened");
+  }
+  
+  form.addEventListener("submit", handleProfileFormSubmit);
