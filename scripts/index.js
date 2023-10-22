308d877edcb8c897f12cc2c4b489bd9e89a663c9
Marina Student <mahbianchi8@gmail.com>
2023-10-22 10:43:07 -0300

    Added fonts

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 let popup = document.querySelector(".popup");
 let editPopupButton = document.querySelector(".profile__edit-button");
 let closePopupButton = document.querySelector(".popup__close-icon");
+let form = document.querySelector(".popup__form");
+let inputName = document.querySelector(".popup__form-input_name"); 
+let inputAbout = document.querySelector(".popup__form-input_about");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__job");
 
 editPopupButton.addEventListener("click", function () {
     popup.classList.add("popup_opened");
-    nameInput.value = profileName.textContent;
-    aboutInput.value = profileAbout.textContent;
+    inputName.value = profileName.textContent;
+    inputAbout.value = profileAbout.textContent;
 });
 
 closePopupButton.addEventListener("click", function() {
     popup.classList.remove("popup_opened")
 });
 
-let form = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__form-input_name"); 
-let aboutInput = document.querySelector(".popup__form-input_about");
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__job");
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-    nameInput.value = profileName.textContent;
-    aboutInput.value = profileAbout.textContent;
+    profileName.textContent = inputName.value;
+    profileAbout.textContent = inputAbout.value;
     popup.classList.remove("popup_opened");
   }
   
