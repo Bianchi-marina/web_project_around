47adf88cbc91902efdd792cd571546a85408e8f1
Marina Student <mahbianchi8@gmail.com>
2023-11-04 10:41:06 -0300

    js

--- scripts/index.js
+++ scripts/index.js
@@ -26,47 +26,59 @@ function handleProfileFormSubmit(evt) {
   
 form.addEventListener("submit", handleProfileFormSubmit);
 
-function openPopup(popup) {
-    popup.classList.add("popup_opened");
-}
-  
-  function closePopup(popup) {
-    popup.classList.remove("popup_opened");
-}
-  
-const popupProductForm = document.querySelector(".popup_product-form");
-const buttonOpenProductForm = document.querySelector(".profile__add-button");
-  buttonOpenProductForm.addEventListener("click", () => {
-    openPopup(popupProductForm);
-});
-function handleImageFormSubmit(evt) {
-    evt.preventDefault();
-    inputTitle.textContent = cardTitle.value;
-    inputImage.src = cardImage.value;
-    popup.classList.remove("popup_opened");
-}
-form.addEventListener("submit", handleImageFormSubmit);
-  
 const addPopupButton = document.querySelector(".profile__add-button");
-const inputTitle = document.querySelector(".popup__form-input_title"); 
+const cardName = document.querySelector(".elements__card-name");
 const inputImage = document.querySelector(".popup__form-input_image");
-const cardTitle = document.querySelector(".elements__card-name");
 const cardImage = document.querySelector(".elements__card-img");
 
-function addCard (cardData) {
-    const templateCard = document.querySelector("#template-card");
-    const cardElement = templateCard.content.cloneNode(true);
+addPopupButton.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+  inputName.value = cardName.textContent;
+  inputImage.value = cardImage.src;
+});
 
-    const nameElement = cardElement.querySelector(".elements__card-name");
-    nameElement.textContent = cardData.name;
+closePopupButton.addEventListener("click", function() {
+  popup.classList.remove("popup_opened")
+});
 
-    const imageElement = cardElement.querySelector(".elements__card-img");
-    imageElement.src = cardData.src;
-    imageElement.alt = cardData.name;
+const formAdd = document.querySelector(".popup__form_add");
+formAdd.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const nameInput = document.querySelector(".popup__form-input_name");
+  const imageInput = document.querySelector(".popup__form-input_image");
 
-    const cardList = document.querySelector(".elements__card");
-    cardList.append(cardElement);
+  const productData = {
+    name: nameInput.value,
+    image: imageInput.value,
+  };
+  addCard(cardData);
+  closePopupButton.addEventListener("click", function() {
+    popup.classList.remove("popup_opened")
+  });
+});
 
+const popupImage = document.querySelector(".popup_image");
+
+function addCard(cardData) {
+  const templateCard = document.querySelector("#template-card");
+  const cardElement = templateCard.content.cloneNode(true);
+
+  const nameElement = cardElement.querySelector(".elements__card-name");
+  nameElement.textContent = cardData.name;
+
+  const imageElement = cardElement.querySelector(".elements__card-img");
+  imageElement.addEventListener("click", () => {
+    const popupImageElement = document.querySelector(".elements__card-img");
+    popupImageElement.src = cardData.image;
+    popupImageElement.alt = cardData.name;
+    openPopup(popupImage);
+  });
+
+  imageElement.src = cardData.image;
+  imageElement.alt = cardData.name;
+
+  const cardList = document.querySelector(".elements__card");
+  cardList.prepend(cardElement);
 }
 
 const initialCards = [
