a1b962828e9119c580f691fbd075973c90902eab
Marina Student <mahbianchi8@gmail.com>
2023-12-08 19:41:31 -0300

    moved files to modules

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,5 +1,78 @@
-//deve conter o código da classe Card.
+export default class Card {
+    constructor(cardData, cardTemplate) {
+      this._name = cardData.name;
+      this._image = cardData.image;
+      this._cardTemplate = cardTemplate;
+    }
+  
+    _getTemplate() {
+      const cardElement = document
+      .querySelector("#template-card")
+      .content
+      .cloneNode(true);
+  
+      return cardElement;
+    }
+  
+    generateCard() {
+      this._element = this._getTemplate();
+      this._element.querySelector(".elements__card-img").src = this._image;
+      this._element.querySelector(".elements__card-name").textContent = this._name;
+      this._element.querySelector(".elements__card-img").alt = this._name;
+      this._setEventListeners();
+      return this._element;
+    }
+  
+    _handleOpenPopup() {
+      const popupImage = this._element
+      .querySelector(".popup_image");
 
-//class Card
-//card = img/text/ icons(like/trash)/ (zoom (popup))
-//metodos de like / trash / generateCard
+      popupImage.classList.add("popup_opened");
+
+      const imageElement =  this._element
+      .querySelector(".popup__img-zoom");
+
+      imageElement.src = this._image;
+      imageElement.alt = this._name;
+
+      const textElement =  this._element
+      .querySelector(".popup__description");
+      
+      textElement.textContent = this._name;
+    }
+    
+    _handleClosePopup() {
+        const popupImage = this._element
+        .querySelector(".popup_image");
+      
+        popupImage.classList.remove("popup_opened");
+    }
+    
+  
+    _setEventListeners() {
+      this._element.addEventListener("click", () => {
+        this._handleOpenPopup();
+      });
+      const closePopupAddButton = this._element
+      .querySelector(".popup__close");
+
+      closePopupAddButton.addEventListener("click", () => {
+        this._handleClosePopup();
+      });
+  
+      const trashButton = this._element
+      .querySelector(".elements__button-trash");
+  
+      trashButton.addEventListener("click", () => {
+      const removeElement = trashButton.closest(".elements__li");
+      removeElement.remove();
+      });
+  
+      const likeButton = this._element
+      .querySelector(".elements__button-like");
+    
+      likeButton.addEventListener("click", () => {
+      likeButton.classList.toggle("elements__button-like_click");
+      });
+    }
+  }
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+import Card  from "./Card.js";
 const popup = document.querySelector(".popup");
 const editPopupButton = document.querySelector(".profile__edit-button");
 const closePopupButton = document.querySelector(".popup__close-icon");
@@ -107,102 +108,8 @@ const initialCards = [
     }
 ];
 
-const popupElement = document.querySelector(".elements__card");
-const popupZoom = document.querySelector(".popup__img-zoom");
-
-function handleOpenPopup() {
-  popupImage.classList.add("popup_opened");
-  const imageElement = document.querySelector(".popup__img-zoom");
-  imageElement.src = this._image;
-  imageElement.alt = this._name;
-  const textElement = document.querySelector(".popup__description");
-  textElement.textContent = this._name;
-}
-
-function handleClosePopup() {
-  popupImage.classList.remove("popup_opened");
-}
-
-class Card {
-  constructor(cardData, cardTemplate) {
-    this._name = cardData.name;
-    this._image = cardData.image;
-    this._cardTemplate = cardTemplate;
-  }
-
-  _getTemplate() {
-    const cardElement = document
-    .querySelector("#template-card")
-    .content
-    .cloneNode(true);
-
-    return cardElement;
-  }
-
-  generateCard() {
-    this._element = this._getTemplate();
-    
-
-    this._element.querySelector(".elements__card-img").src = this._image;
-    this._element.querySelector(".elements__card-name").textContent = this._name;
-    this._element.querySelector(".elements__card-img").alt = this._name;
-
-    this._setEventListeners();
-
-    return this._element;
-  }
-
-  _setEventListeners() {
-    this._element.addEventListener("click", () => {
-      this.handleOpenPopup();
-    });
-
-    closePopupButton.addEventListener("click", () => {
-      this.handleClosePopup();
-    });
-
-    const trashButton = this._element
-    .querySelector(".elements__button-trash");
-
-    trashButton.addEventListener("click", () => {
-    const removeElement = trashButton.closest(".elements__li");
-    removeElement.remove();
-    });
-
-    const likeButton = this._element
-    .querySelector(".elements__button-like");
-  
-    likeButton.addEventListener("click", () => {
-    likeButton.classList.toggle("elements__button-like_click");
-    });
-  }
-}
-
 initialCards.forEach((item) => {
   const card = new Card (item, "#template-card");
   const cardElement = card.generateCard();
   popupElement.prepend(cardElement);
 });
-
-// const imageElement = cardElement.querySelector(".elements__card-img");
-// imageElement.src = cardData.image;
-// imageElement.alt = cardData.name;
-
-// imageElement.addEventListener("click", () => {
-//   const popupZoom = document.querySelector(".popup__img-zoom");
-//   popupZoom.src = cardData.image;
-//   const popupDescription = document.querySelector(".popup__description");
-//   popupDescription.textContent = cardData.name;
-//   popupImage.classList.add("popup_opened");
-// });
-
-// const closeZoom = document.querySelector(".popup__close-zoom");
-// closeZoom.addEventListener("click", () => {
-//   popupImage.classList.remove("popup_opened");
-// })
-
-
-
-//As classes Card e FormValidator devem ser exportadas de seus arquivos correspondentes,
-// e então você deve importá-las e implementá-las dentro do index.js.
-//Certifique-se de que os arquivos JS estejam conectados ao index.html como módulos.
